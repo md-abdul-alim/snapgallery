7c087019579937ac60399446538941c1f8989ef6
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2024-08-08 17:05:09 +0600

    initial navbar

---
+++ TAILWINDCSS.md
@@ -0,0 +1,29 @@
+# Space Between ([text](https://tailwindcss.com/docs/space)):
+    ~ space-x-8 (space start from 2nd step): margin-left: 2rem; /* 32px */
+    ~ space-y-8 : margin-top: 2rem; /* 32px */
+    ~ space-x-px : margin-left: 1px;
+    ~ space-y-px : margin-top: 1px;
+    ~ Reversing children order:
+        ~ "flex flex-row-reverse space-x-4 space-x-reverse"
+    ~ Using negative values:
+        ~ "-space-x-4"
+    ~ Custom space value:
+        ~ space-x-[15px]
+
+# Height
+    ~ h-0 : height: 0px
+    ~ h-px : height: 1px
+    ~ h-3 : height: 0.75rem
+    ~ h-auto : height: auto
+    ~ h-full : height: 100%
+    ~ h-screen : height: 100vh;
+    ~ h-min	height: min-content;
+    ~ h-max	height: max-content;
+    ~ h-fit	height: fit-content;
+
+# Position ([text](https://tailwindcss.com/docs/position))
+    ~ static	position: static;
+    ~ fixed	position: fixed;
+    ~ absolute	position: absolute;
+    ~ relative	position: relative;
+    ~ sticky	position: sticky;
---
+++ app/components/Navbar.js
@@ -0,0 +1,53 @@
+'use client'
+
+import React, { useState } from 'react'
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from '@headlessui/react'
+
+import {navigation} from './navigation'
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className='bg-white'>
+      <header>
+        <nav>
+          <PopoverGroup>
+            {
+              navigation.categories.map((category) => (
+                <Popover key={category.id} className="relative flex">
+                  <PopoverButton>
+                    {category.name}
+                  </PopoverButton>
+
+                  <PopoverPanel>
+                    {
+                      category.featured.map((item) => (
+                        <div key={item.name}>
+                          <a href={item.href}>{item.name}</a>
+                        </div>
+                      ))
+                    }
+                  </PopoverPanel>
+                </Popover>
+              ))
+            }
+          </PopoverGroup>
+        </nav>
+      </header>
+    </div>
+  )
+}
