ff5a8e7d23c7d56f63e02039ecf204d20e1b756d
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2024-08-14 17:53:08 +0600

    initial image view desig done

--- app/components/image/ImageGrid.js
+++ app/components/image/ImageGrid.js
@@ -1,3 +1,30 @@
+// import Image from "next/image";
+// import React from "react";
+
+// export default function ImageGrid({ images }) {
+//   return (
+//     <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+//       <div
+//         className="columns-1 sm:columns-1 md:columns-2 lg:columns-3 gap-6 space-y-6"
+//         style={{ columnGap: '16px' }} // Adjust the gap as needed
+//       >
+//         {images.map((image) => (
+//           <div key={image.id} className="relative mb-6 break-inside-avoid">
+//             <Image
+//               placeholder="blur"
+//               src={image.url}
+//               alt={image.alt}
+//               loading="lazy"
+//               quality={100}
+//               className="w-full h-auto object-cover rounded-lg shadow-lg"
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
 import Image from "next/image";
 
 export default function ImageGrid({ images }) {
@@ -18,4 +45,3 @@ export default function ImageGrid({ images }) {
       </div>
     );
   }
-  
